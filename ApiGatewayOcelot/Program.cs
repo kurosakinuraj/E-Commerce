780da22f89117f2e3be1499b1bb8981472708d9b
kurosakinuraj <daemoncodenuraj@gmail.com>
2023-08-21 12:37:06 +0545

    Ocelot Gate Authentication implementation

--- ApiGatewayOcelot/Program.cs
+++ ApiGatewayOcelot/Program.cs
@@ -1,11 +1,18 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using JwtAuthManager;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
 builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddCustomJwtAuthentication();
 var app = builder.Build();
 await app.UseOcelot();
+
+//authentication
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.Run();
